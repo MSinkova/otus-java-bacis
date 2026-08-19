4bcddaab5eae1dac2024f58ec31078fd2cb933a0
MSinkova <msinkova92@gmail.com>
2026-08-19 22:42:15 +0300

    add homework1

--- src/main/java/ru/otus/java/basic/Otus.java
+++ src/main/java/ru/otus/java/basic/Otus.java
@@ -5,6 +5,63 @@ import java.util.Scanner;
 public class Otus {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        do {
+            System.out.println("Введите число от -5 до 5");
+            n = scanner.nextInt();
+        } while (n <-5 || n > 5);
+        System.out.println("Вы ввели число: " + n);
 
+        greetings();
+        checkColor(n, n, n);
+        selectColor();
+        compareNumbers();
+        addOrSubtractAndPrint(n,n,false);
+    }
+
+    public static void greetings() {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+
+    public static void checkColor(int a, int b, int c) {
+        int result = a + b + c;
+        if(result >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void selectColor() {
+        int data = 21;
+        if (data <= 10) {
+            System.out.println("Красный");
+        } else if (data > 10 && data <= 20) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 1;
+        int b = 7;
+        if(a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+    public static void addOrSubtractAndPrint(int initValue, int delta, boolean increment) {
+        if (increment == true) {
+            System.out.println(initValue + delta);
+        } else {
+            System.out.println(initValue - delta);
+        }
     }
 }
